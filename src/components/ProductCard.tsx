f6ea10732adcd83ee1f90de6029b0f14526f508c
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-26 04:12:10 -0500

    ProductCard compoennt outline

---
+++ src/components/ProductCard.tsx
@@ -0,0 +1,23 @@
+import '../styles/ProductCard.css';
+
+interface Props {
+	img: string;
+	title: string;
+	processor: string;
+	description: string;
+	price: number;
+}
+
+const ProductCard = ({ img, title, processor, description, price }: Props) => {
+	return (
+		<div className="card">
+			<img className="card-product" src={img} alt="Product Image" />
+			<h3 className="card-title">{title}</h3>
+			<p className="card-processor">{processor}</p>
+			<p className="card-description">{description}</p>
+			<p className="card-price">${price}</p>
+		</div>
+	);
+};
+
+export default ProductCard;
