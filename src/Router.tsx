e3253229dde31cc45e61c318a3255acd7f5804d6
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-06-26 03:06:49 -0500

    Add routes for new components

--- src/Router.tsx
+++ src/Router.tsx
@@ -6,33 +6,57 @@ import NotFound from './components/NotFound';
 import appleLogo from './assets/black-apple-logo.png';
 import shoppingCart from './assets/shoppingBag.svg';
 import githubLogo from './assets/githubLogo.png';
+import Mac from './components/Mac';
+import Iphone from './components/Iphone';
+import Ipad from './components/Ipad';
+import AppleWatch from './components/AppleWatch';
 
 const Router = () => {
 	return (
 		<>
-			<div className="navBar">
-				<Link to={'/'}>
-					<img src={appleLogo} alt="test" />
-				</Link>
-				<div className="navLinks">
+			<div className="main-nav">
+				<div className="navBar">
 					<Link to={'/'}>
-						<p className="navText">Home</p>
+						<img src={appleLogo} alt="test" />
 					</Link>
-					<Link to={'/shop'}>
-						<p className="navText">Store</p>
-					</Link>
-					<Link to={'/checkout'}>
-						<p className="navText">Checkout</p>
-					</Link>
-					<Link to={'/checkout'}>
-						<img src={shoppingCart} alt="shopping cart" />
-						<p className="cart-count">0</p>
-					</Link>
+					<div className="navLinks">
+						<Link to={'/'}>
+							<p className="navText">Home</p>
+						</Link>
+						<Link to={'/shop'}>
+							<p className="navText">Store</p>
+						</Link>
+						<div className="products">
+							<Link to={'/mac'}>
+								<p className="productText">Mac</p>
+							</Link>
+							<Link to={'/iphone'}>
+								<p className="productText">Iphone</p>
+							</Link>
+							<Link to={'/ipad'}>
+								<p className="productText">Ipad</p>
+							</Link>
+							<Link to={'/watch'}>
+								<p className="productText">Watch</p>
+							</Link>
+						</div>
+						<Link to={'/checkout'}>
+							<p className="navText">Checkout</p>
+						</Link>
+						<Link to={'/checkout'}>
+							<img src={shoppingCart} alt="shopping cart" />
+							<p className="cart-count">0</p>
+						</Link>
+					</div>
 				</div>
 			</div>
 			<Routes>
 				<Route path="/" Component={Home} />
 				<Route path="/shop" Component={Shop} />
+				<Route path="/mac" Component={Mac} />
+				<Route path="/iphone" Component={Iphone} />
+				<Route path="/ipad" Component={Ipad} />
+				<Route path="/watch" Component={AppleWatch} />
 				<Route path="/checkout" Component={Checkout} />
 				<Route path="*" Component={NotFound} />
 			</Routes>
