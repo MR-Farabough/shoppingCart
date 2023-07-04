de43b7bf6b85431e1c654450f63e5e1be8033f45
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-07-04 08:04:40 -0500

    update path

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://michael-lgtm.github.io/shoppingCart/',
+  base: 'https://michael-lgtm.github.io/shoppingCart//',
   plugins: [react()],
 })
