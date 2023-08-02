f972ce9eb2906690fc3612a291cf3da7a8be38b3
Michael <119344018+Michael-lgtm@users.noreply.github.com>
2023-08-02 10:09:39 -0500

    Built App

--- src/Router.tsx
+++ src/Router.tsx
@@ -14,45 +14,80 @@ import { useState } from 'react';
 const Router = () => {
 	let [cart, setCart] = useState([]);
 	let [totalCart, setTotalCart] = useState<{}[]>([]);
+	const [mobileNav, setMobileNav] = useState(false);
 	if (cart.length > 0) {
 		setTotalCart([...totalCart, cart]);
 		setCart([]);
 	}
+	const resizeNav = () => {
+		if (window.innerWidth < 650) {
+			setMobileNav(true);
+		} else {
+			setMobileNav(false);
+		}
+	};
+	window.addEventListener('resize', resizeNav);
 	return (
 		<>
-			<div className="main-nav">
-				<div className="navBar">
-					<Link to={'/'}>
-						<img src={appleLogo} alt="test" />
-					</Link>
-					<div className="navLinks">
+			{!mobileNav && (
+				<div className="main-nav">
+					<div className="navBar">
 						<Link to={'/'}>
-							<p className="navText">Home</p>
+							<img src={appleLogo} alt="test" />
 						</Link>
-						<Link to={'/shop'}>
-							<p className="navText">Store</p>
-						</Link>
-						<div className="products">
-							<Link to={'/mac'}>
-								<p className="productText">Mac</p>
+						<div className="navLinks">
+							<Link to={'/'}>
+								<p className="navText">Home</p>
 							</Link>
-							<Link to={'/iphone'}>
-								<p className="productText">Iphone</p>
+							<Link to={'/shop'}>
+								<p className="navText">Store</p>
 							</Link>
-							<Link to={'/watch'}>
-								<p className="productText">Watch</p>
+							<div className="products">
+								<Link to={'/mac'}>
+									<p className="productText">Mac</p>
+								</Link>
+								<Link to={'/iphone'}>
+									<p className="productText">Iphone</p>
+								</Link>
+								<Link to={'/watch'}>
+									<p className="productText">Watch</p>
+								</Link>
+							</div>
+							<Link to={'/checkout'}>
+								<p className="navText">Checkout</p>
+							</Link>
+							<Link to={'/checkout'}>
+								<img src={shoppingCart} alt="shopping cart" />
+								<p className="cart-count">{totalCart.length}</p>
 							</Link>
 						</div>
-						<Link to={'/checkout'}>
-							<p className="navText">Checkout</p>
-						</Link>
-						<Link to={'/checkout'}>
-							<img src={shoppingCart} alt="shopping cart" />
-							<p className="cart-count">{totalCart.length}</p>
-						</Link>
 					</div>
 				</div>
-			</div>
+			)}
+			{mobileNav && (
+				<div className="main-nav">
+					<div className="navBar">
+						<Link to={'/'}>
+							<img src={appleLogo} alt="test" />
+						</Link>
+						<div className="navLinks">
+							<Link to={'/'}>
+								<p className="navText">Home</p>
+							</Link>
+							<Link to={'/shop'}>
+								<p className="navText">Store</p>
+							</Link>
+							<Link to={'/checkout'}>
+								<p className="navText">Checkout</p>
+							</Link>
+							<Link to={'/checkout'}>
+								<img src={shoppingCart} alt="shopping cart" />
+								<p className="cart-count">{totalCart.length}</p>
+							</Link>
+						</div>
+					</div>
+				</div>
+			)}
 			<Routes>
 				<Route path="/" element={<Home />} />
 				<Route
